1e1751541bc237fc48f106e2ad2a575647a1a7bf
Aphinat.f <106942398+Aphinat4502@users.noreply.github.com>
2023-01-20 20:57:31 +0700

    Update main.php

--- main.php
+++ main.php
@@ -6,12 +6,12 @@
     <h1>แบบสอบถาม</h1>
     <form action="show.php" method="get" style="margin-left: 80px;">
     ชื่อ:<input type="text" name="name"><br>
-        กรุณาระบุเพศ:
+        โปรดระบุเพศ:
         <select name="gender">
             <option value="male">ชาย</option>
             <option value="female">หญิง</option>
         </select><br>
-        กรุณาใส่อายุ:
+        โปรดใส่อายุ:
         <input type="number" name="num" style="width: 40px;"><br>
         สถานะ:
         <select name="status">
